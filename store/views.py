b7dc0de812278fec1ccd8a9d3280ae57e8dc5b10
JieLuis <104972485+JieLuis@users.noreply.github.com>
2024-05-13 19:15:51 +0800

    Implement pagination and ordering

--- store/views.py
+++ store/views.py
@@ -1,16 +1,19 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response 
 from rest_framework.viewsets import ModelViewSet
+from .pagination import DefaultPagination
 from .filters import ProductFilter
 from .models import Product, OrderItem, Review
 from .serializers import ProductSerializer, ReviewSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     # def get_queryset(self):
     #     qeuryset = Product.objects.all()
