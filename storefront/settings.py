1aa866a30c8a1a26806f6c6f70d300961506f91b
JieLuis <104972485+JieLuis@users.noreply.github.com>
2024-05-17 18:51:59 +0800

    Create a email server

--- storefront/settings.py
+++ storefront/settings.py
@@ -177,3 +177,10 @@ DOJSER = {
     }
 }
  
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST =  'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'from@moshy.com'
