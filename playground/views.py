b1d608b8d65fce1bc5cb6026755fa1bb522b4660
JieLuis <104972485+JieLuis@users.noreply.github.com>
2024-05-18 12:24:50 +0800

    Set up redis and celery

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,15 @@
 from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
     try:
-        send_mail('subject', 'message', 'from@moshy.com', ['bob@hotmail.com'])
+        message =BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name' : 'Jie'}
+        )
+        message.send(['John@moshby.com']) 
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -184,3 +184,5 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
 DEFAULT_FROM_EMAIL = 'from@moshy.com'
+
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
