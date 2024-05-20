5ca88c5a66994edfdbd304c28a0aa90214261268
JieLuis <104972485+JieLuis@users.noreply.github.com>
2024-05-20 18:52:33 +0800

    Add redis for caching

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,31 @@
 from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page 
+from django.core.cache import cache 
+from rest_framework.views import  APIView
+from rest_framework.request import Request
 from templated_mail.mail import BaseEmailMessage
 from .tasks import notify_customers
 import requests
 
-def say_hello(request):
-    # notify_customers.delay('Hello')
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request : Request):
+        response : requests.Response = requests.get('https://httpbin.org/delay/0')
+        data = response.json()
+        # print(request)
+        return render(request, 'hello.html', {'name': 'Jie'}) 
 
-    requests.get('https://httpbin.org/delay/2 ')
-    return render(request, 'hello.html', {'name': 'Mosh'})
+
+# @cache_page(5 * 60)
+# def say_hello(request):
+#     # notify_customers.delay('Hello')
+ 
+#     response : requests.Response = requests.get('https://httpbin.org/delay/2')
+#     print(request)
+#     data = response.json()
+#     return render(request, 'hello.html', {'name': 'Jie'}) 
 
 
  
