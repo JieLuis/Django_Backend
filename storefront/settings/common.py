6df99ec374c56007a4534ca731ebfaf17215fe83
JieLuis <104972485+JieLuis@users.noreply.github.com>
2024-05-22 02:06:38 +0800

    reday for production

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -158,13 +158,7 @@ DOJSER = {
  
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST =  'localhost'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 2525
-DEFAULT_FROM_EMAIL = 'from@moshy.com'
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_BEAT_SCHEDULE = {
     'notify_customers' : {
         'task' : 'playground.tasks.notify_customers',
@@ -172,18 +166,6 @@ CELERY_BEAT_SCHEDULE = {
         'args' : ['hello world']
     }
 }
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-} 
-import os
-
 
 LOGGING = {
     'version': 1,
